6595e8c4a50c9b8bab72aed3ad355538a5e92a63
Jordan Doyle <jordan@doyle.la>
2024-11-13 02:16:15 +0000

    Remove intermediary vecs/btreemaps for ordering on index

--- src/methods/index.rs
+++ src/methods/index.rs
@@ -1,8 +1,12 @@
-use std::{collections::BTreeMap, sync::Arc};
+use std::{cell::RefCell, sync::Arc};
 
 use anyhow::Context;
 use askama::Template;
-use axum::{response::IntoResponse, Extension};
+use axum::{
+    response::{IntoResponse, Response},
+    Extension,
+};
+use itertools::{Either, Itertools};
 
 use super::filters;
 use crate::{
@@ -12,28 +16,45 @@ use crate::{
 
 #[derive(Template)]
 #[template(path = "index.html")]
-pub struct View {
-    pub repositories: BTreeMap<Option<String>, Vec<YokedRepository>>,
+pub struct View<
+    'a,
+    Group: Iterator<Item = (&'a String, &'a YokedRepository)>,
+    GroupIter: Iterator<Item = (&'a str, Group)>,
+> {
+    // this type sig is a necessary evil unfortunately, because askama takes a reference
+    // to the data for rendering.
+    pub repositories: RefCell<Either<GroupIter, std::iter::Empty<(&'a str, Group)>>>,
+}
+
+impl<'a, Group, GroupIter> View<'a, Group, GroupIter>
+where
+    Group: Iterator<Item = (&'a String, &'a YokedRepository)>,
+    GroupIter: Iterator<Item = (&'a str, Group)>,
+{
+    fn take_iter(&self) -> Either<GroupIter, std::iter::Empty<(&'a str, Group)>> {
+        self.repositories.replace(Either::Right(std::iter::empty()))
+    }
 }
 
 pub async fn handle(
     Extension(db): Extension<Arc<rocksdb::DB>>,
-) -> Result<impl IntoResponse, super::repo::Error> {
-    let mut repositories: BTreeMap<Option<String>, Vec<YokedRepository>> = BTreeMap::new();
-
+) -> Result<Response, super::repo::Error> {
     let fetched = tokio::task::spawn_blocking(move || Repository::fetch_all(&db))
         .await
         .context("Failed to join Tokio task")??;
 
-    for (k, v) in fetched {
-        // TODO: fixme
-        let mut split: Vec<_> = k.split('/').collect();
-        split.pop();
-        let key = Some(split.join("/")).filter(|v| !v.is_empty());
+    // rocksdb returned the keys already ordered for us so group_by is a nice
+    // operation we can use here to avoid writing into a map to group. though,
+    // now that i think about it it might act a little bit strangely when mixing
+    // root repositories and nested repositories. we're going to have to prefix
+    // root repositories with a null byte or something. i'll just leave this here
+    // as a TODO.
+    let repositories = fetched
+        .iter()
+        .group_by(|(k, _)| memchr::memrchr(b'/', k.as_bytes()).map_or("", |idx| &k[..idx]));
 
-        let k = repositories.entry(key).or_default();
-        k.push(v);
-    }
-
-    Ok(into_response(View { repositories }))
+    Ok(into_response(View {
+        repositories: Either::Left(repositories.into_iter()).into(),
+    })
+    .into_response())
 }

b13b80405d4b490d01a7be34835d439e82a60397
Jordan Doyle <jordan@doyle.la>
2025-01-05 23:02:00 +0700

    Use forked helix when building without TREE_SITTER_GRAMMAR_LIB_DIR

--- tree-sitter-grammar-repository/build.rs
+++ tree-sitter-grammar-repository/build.rs
@@ -14,8 +14,8 @@ use quote::{format_ident, quote};
 use serde::Deserialize;
 use threadpool::ThreadPool;
 
-const GRAMMAR_REPOSITORY_URL: &str = "https://github.com/helix-editor/helix";
-const GRAMMAR_REPOSITORY_REF: &str = "82dd96369302f60a9c83a2d54d021458f82bcd36";
+const GRAMMAR_REPOSITORY_URL: &str = "https://github.com/JordanForks/helix";
+const GRAMMAR_REPOSITORY_REF: &str = "1603715cc91bf6fdffb4aedfb5b76fb69fd10e28";
 const GRAMMAR_REPOSITORY_CONFIG_PATH: &str = "languages.toml";
 
 static BLACKLISTED_MODULES: &[&str] = &[
